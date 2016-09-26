ef2fa638e9878398eba5c193c9300643932639de
t3rr0r <mail@t3rr0r.com>
2016-09-26 09:45:56 -0400

    fixed current week bug

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 IP=52.9.51.222
 
 deploy:
+	cap all deploy
+	make publish
+
+publish:
 	grunt production
 	rsync -av --delete ./index.html deploy@$(IP):/home/deploy/power_rankings/current/index.html
 
--- utils.js
+++ utils.js
@@ -19,27 +19,27 @@ var should_lock = function() {
 var getWeek = function() {
   var now = moment()
 
-  if (now.isBetween('2016-09-20', '2016-09-26', null, '[]'))
+  if (now.isBetween('2016-09-20', '2016-09-26', 'day', '[]'))
     return 3
-  if (now.isBetween('2016-09-27', '2016-10-03', null, '[]'))
+  if (now.isBetween('2016-09-27', '2016-10-03', 'day', '[]'))
     return 4
-  if (now.isBetween('2016-10-04', '2016-10-10', null, '[]'))
+  if (now.isBetween('2016-10-04', '2016-10-10', 'day', '[]'))
     return 5
-  if (now.isBetween('2016-10-11', '2016-10-17', null, '[]'))
+  if (now.isBetween('2016-10-11', '2016-10-17', 'day', '[]'))
     return 6
-  if (now.isBetween('2016-10-18', '2016-10-24', null, '[]'))
+  if (now.isBetween('2016-10-18', '2016-10-24', 'day', '[]'))
     return 7
-  if (now.isBetween('2016-10-25', '2016-10-31', null, '[]'))
+  if (now.isBetween('2016-10-25', '2016-10-31', 'day', '[]'))
     return 8
-  if (now.isBetween('2016-11-01', '2016-11-07', null, '[]'))
+  if (now.isBetween('2016-11-01', '2016-11-07', 'day', '[]'))
     return 9
-  if (now.isBetween('2016-11-08', '2016-11-14', null, '[]'))
+  if (now.isBetween('2016-11-08', '2016-11-14', 'day', '[]'))
     return 10
-  if (now.isBetween('2016-11-15', '2016-11-21', null, '[]'))
+  if (now.isBetween('2016-11-15', '2016-11-21', 'day', '[]'))
     return 11
-  if (now.isBetween('2016-11-22', '2016-11-28', null, '[]'))
+  if (now.isBetween('2016-11-22', '2016-11-28', 'day', '[]'))
     return 12
-  if (now.isBetween('2016-11-29', '2016-12-05', null, '[]'))
+  if (now.isBetween('2016-11-29', '2016-12-05', 'day', '[]'))
     return 13
 }
 
